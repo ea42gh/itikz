661e9b383950cae60ec364dd81968b93a2462bb4
ea42gh <ea42_github@mail.com>
2025-12-18 08:37:20 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,60 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Thin shim for setuptools.
 
-"""The setup script."""
+All package metadata is now stored in setup.cfg and pyproject.toml.
+"""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = []
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest', ]
-
-setup(
-    author="John Bjorn Nelson",
-    author_email='jbn@abreka.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-    ],
-    description="Cell magic for PGF/TikZ-to-SVG rendering in Jupyter",
-    entry_points={
-        'console_scripts': [
-            'itikz=itikz.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='itikz',
-    name='itikz',
-    packages=find_packages(include=['itikz']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/ea42gh/itikz',
-    version='0.1.6',
-    zip_safe=False,
-)
+setup()
